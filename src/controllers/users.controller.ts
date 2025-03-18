2e41d8480251da143b3d660afe16f54e6c440cea
julioCarrilloDEV <jc.carrillodev@gmail.com>
2025-03-18 17:06:22 -0300

    implementando tratamento de erros nas rotas de usuários

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,54 +1,74 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { getFirestore } from "firebase-admin/firestore";
 
 
 export class UsersController {
-    static async getAll(req: Request, res: Response){
-        const snapshot = await getFirestore().collection("users").get(); //snapshot contém todos os documentos encontrados.
-        const users = snapshot.docs.map(doc => { //map percorre todos os documentos e retorna um array com os dados de cada documento
-            return {
-                id: doc.id,
-                ...doc.data() //adiciona todos os campos do documento (nome, email, etc.).
-            };
-        })
-        res.send(users)
-    }
-
-    static async getById(req: Request, res: Response){
-        let userId = req.params.id;
-        const doc = await getFirestore().collection("users").doc(userId).get(); //.doc(userId).get() → Retorna apenas o documento com o ID especificado
-        let user = {
-            id: doc.id,
-            ...doc.data()
+    static async getAll(req: Request, res: Response, next: NextFunction){
+        try{
+                const snapshot = await getFirestore().collection("users").get(); //snapshot contém todos os documentos encontrados.
+                const users = snapshot.docs.map(doc => { //map percorre todos os documentos e retorna um array com os dados de cada documento
+                return {
+                    id: doc.id,
+                    ...doc.data() //adiciona todos os campos do documento (nome, email, etc.).
+                };
+            })
+            res.send(users)
+        } catch(error){
+            next(error);
         }
-        res.send(user);
     }
 
-    static async save(req: Request, res: Response){
-        let user = req.body;
-        const userSalvo = await getFirestore().collection("users").add(user); //Adiciona um usuário no Firestore
-        res.send({
-            message: `Usuário ${userSalvo.id} salvo com sucesso`
-        })
+    static async getById(req: Request, res: Response, next: NextFunction){
+        try {
+            let userId = req.params.id;
+            const doc = await getFirestore().collection("users").doc(userId).get(); //.doc(userId).get() → Retorna apenas o documento com o ID especificado
+            let user = {
+                id: doc.id,
+                ...doc.data()
+            }
+            res.send(user);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    static async save(req: Request, res: Response, next: NextFunction){
+        try {
+            let user = req.body;
+            const userSalvo = await getFirestore().collection("users").add(user); //Adiciona um usuário no Firestore
+            res.send({
+                message: `Usuário ${userSalvo.id} salvo com sucesso`
+            })
+        } catch (error) {
+            next(error);
+        }
     }
     
-    static update(req: Request, res:Response){
-        let userId = req.params.id;
-        let user = req.body;
-        getFirestore().collection("users").doc(userId).set({ //set() atualiza um documento no Firestore
-            nome: user.nome,
-            email: user.email
-        })
-        res.send({
-            message: "Usuário alterado com sucesso"
-        })
+    static update(req: Request, res:Response, next: NextFunction){
+        try {
+            let userId = req.params.id;
+            let user = req.body;
+            getFirestore().collection("users").doc(userId).set({ //set() atualiza um documento no Firestore
+                nome: user.nome,
+                email: user.email
+            })
+            res.send({
+                message: "Usuário alterado com sucesso"
+            })
+        } catch (error) {
+            next(error);
+        }
     }
     
-    static async delete(req: Request, res: Response){
-        let userId = req.params.id;
-        await getFirestore().collection("users").doc(userId).delete(); //delete() remove um documento do Firestore
-        res.send({
-            message: "Usuário removido com sucesso"
-        })
+    static async delete(req: Request, res: Response, next: NextFunction){
+        try {
+            let userId = req.params.id;
+            await getFirestore().collection("users").doc(userId).delete(); //delete() remove um documento do Firestore
+            res.send({
+                message: "Usuário removido com sucesso"
+            })
+        } catch (error) {
+            next(error);
+        }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import {routes} from './routes/index';
 import { initializeApp } from 'firebase-admin/app';
 
@@ -6,7 +6,11 @@ initializeApp();
 const app = express();
 
 routes(app);
-
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).send({
+        message: "Erro interno do servidor"
+    })
+})
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
 })
